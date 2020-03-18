7746700e21593c28c976836f476537d7965abad5
Faheem Kaskar <kfaheem@Faheems-MacBook-Pro.local>
2020-03-18 16:52:16 -0400

    Added pandas operations

---
+++ python/pandas/pandas.py
@@ -0,0 +1,72 @@
+import pandas as pd
+
+
+# Creating dataframe from object==========================================
+
+obj1 = [{}, {}, {}]
+df1 = pd.DataFrame(obj1, dtype={"col1": "str", "col2": "int", "col3": "bool"})
+
+obj2 = {"a": [], "b": [], "c": []}
+df2 = pd.DataFrame(obj2)
+
+df3 = pd.read_csv("")
+
+df4 = pd.read_json("")
+
+
+#  Create object from dataframe ==========================================
+
+obj3 = df1.to_dict(orient="records")
+
+csv_path = ""
+obj4 = df3.to_csv(csv_path, index=True, header=True)
+
+obj5 = df4.to_json()
+
+# Dataframe operations ==========================================
+
+df5 = df1[["col1", "col2"]][df1["col3"].isna]
+
+df6 = df1[["col1", "col2"]][df1["col3"].notna]
+
+df7 = df1[["col1", "col2"]][df1["col3"] == "xyz"]
+
+df8 = df7.drop_duplicates(subset=["col1", "col2"])
+
+df9 = df8[5:10]  # selects rows 5 to 10 and creates a new df
+
+df10 = df9.groupby("col1").agg(Mean=("col2", "mean"), Count=("col3", "count")).query("Mean > 3 & Count <=10")
+
+df11 = df9.groupby("col1").agg({"col1": ["max", "min", "median"]})
+
+df11["col10"].between("time1", "time2")
+
+df11.sort_values("col2", ascending=False)
+
+df11.head(), df11.tail(), df11.take(10), df11.nlargest(5), df11.nsmallest(5), df11.nunique(5)
+
+df11["col1"].values("index") or df11.values("index")  # returns numpy array
+
+df11.dtype.value_counts()
+df11["col2"].value_counts()  # value_counts() does not work on a df, it needs a pandas series
+
+# Dataframe loops ==========================================
+
+for _, row in df11.iterrows():
+    row.col_new = row.col1 + row.col2
+
+for row in df11.itertuples():
+    row.col_new = row.col1 + row.col2
+
+for index in range(len(df11)): # faster than above 2 loops
+    df11["new_col"] = df11["col1"].iloc[index] + df11["col2"].iloc[index]
+
+df11["new_col"] = (df11["col1"] + df11["col2"]).sum()  # fastest
+
+# Pandas Series ==========================================
+
+obj6 = [1, 2, 3, 4, 4, 5]
+
+ser = pd.Series(obj6, dtype="str")
+
+ser.isdigit(), ser.split(), ser.lower()  # basically all string operations

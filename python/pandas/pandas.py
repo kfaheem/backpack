29b20165e7bb8739ef1d25ead1fe29ebc8a3e108
Faheem Kaskar <kfaheem@Faheems-MacBook-Pro.local>
2020-03-18 17:49:38 -0400

    Added pandas operations

--- python/pandas/pandas.py
+++ python/pandas/pandas.py
@@ -33,7 +33,13 @@ df7 = df1[["col1", "col2"]][df1["col3"] == "xyz"]
 
 df8 = df7.drop_duplicates(subset=["col1", "col2"])
 
+df78 = df7.drop_na(subset="col9")
+
+df89 = df7["col6"].to_frame()
+
 df9 = df8[5:10]  # selects rows 5 to 10 and creates a new df
+
+df = df9.groupby("col1")["col5"].mean()
 
 df10 = df9.groupby("col1").agg(Mean=("col2", "mean"), Count=("col3", "count")).query("Mean > 3 & Count <=10")
 
@@ -49,6 +55,17 @@ df11["col1"].values("index") or df11.values("index")  # returns numpy array
 
 df11.dtype.value_counts()
 df11["col2"].value_counts()  # value_counts() does not work on a df, it needs a pandas series
+
+
+df12 = pd.concat([df1, df2, df3], axis=0)  # one below another
+
+df13 = pd.concat([df1, df2, df3], axis=1)  # side-by-side
+
+df14 = df13.T  # Transpose
+
+df15 = df14.stack()
+
+df15["new_col"] = df15.apply(lambda x: x["cola"] - x["colb"], axis=1)
 
 # Dataframe loops ==========================================
 

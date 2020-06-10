b4bf1a99c9fb048a386a2783b90f0347c142a798
Faheem Kaskar <kfaheem@Faheems-MacBook-Pro.local>
2020-06-09 20:20:04 -0400

    spark project

--- DataEngineerNanoDegree/Project4-spark/etl.py
+++ DataEngineerNanoDegree/Project4-spark/etl.py
@@ -21,8 +21,15 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
+    """
+
+    :param spark:
+    :param input_data:
+    :param output_data:
+    :return:
+    """
     # get filepath to song data file
-    song_data = "{}/song_data/".format(input_data)
+    song_data = "{}/song_data/*".format(input_data)
 
     # read song data file
     df = spark.read.json(song_data)
@@ -42,14 +49,24 @@ def process_song_data(spark, input_data, output_data):
 
 
 def process_log_data(spark, input_data, output_data):
+    """
+
+    :param spark:
+    :param input_data:
+    :param output_data:
+    :return:
+    """
     # get filepath to log data file
-    log_data = "{}/log_data/".format(input_data)
+    log_data = "{}/log_data/*".format(input_data)
 
     # read log data file
     df = spark.read.json(log_data)
 
     # filter by actions for song plays
     df = df.where("page"=="NextSong")
+
+    # log_table temp_view
+    df.createOrReplaceTempView("log_table")
 
     # extract columns for users table
     users_table = df.selectExpr("""SELECT userId AS user_id, firstName AS first_name, 
@@ -59,33 +76,59 @@ def process_log_data(spark, input_data, output_data):
     users_table.write.parquet("{}/users.parquet".format(output_data))
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf(lambda x: x*1000)
-    df = df.withColumn("timeStamp", get_timestamp("ts"))
+    # get_timestamp = udf(lambda x: x*1000)
+    # df = df.withColumn("timeStamp", get_timestamp("ts"))
 
     # create datetime column from original timestamp column
-    get_datetime = udf(lambda x: x*1)
-    df = df.select([year(""), month(""), dayofmonth(""), hour(""), weekofyear(""), date_format("")])
+    # get_datetime = udf(lambda x: x*1)
+    # df = df.select([year(""), month(""), dayofmonth(""), hour(""), weekofyear(""), date_format("")])
 
     # extract columns to create time table
-    time_table =
+    time_table = spark.sql("""
+    SELECT  DISTINCT TIMESTAMP 'epoch' + se.ts/1000 \
+    * INTERVAL '1 second'        AS start_time,
+    EXTRACT(hour FROM start_time)    AS hour,
+    EXTRACT(day FROM start_time)     AS day,
+    EXTRACT(week FROM start_time)    AS week,
+    EXTRACT(month FROM start_time)   AS month,
+    EXTRACT(year FROM start_time)    AS year,
+    EXTRACT(week FROM start_time)    AS weekday
+    FROM    log_table                AS se;
+    """)
 
     # write time table to parquet files partitioned by year and month
     time_table.write.partitionBy("year", "month").parquet("{}/time.parquet".format(output_data))
 
     # read in song data to use for songplays table
-    song_df =
+    song_df = spark.read.parquet("{}/songs.parquet".format(output_data))
+
+    # song_table temp view
+    song_df.createOrReplaceTempView("song_table")
 
     # extract columns from joined song and log datasets to create songplays table
-    songplays_table =
+    songplays_table = spark.sql("""
+    SELECT  DISTINCT TIMESTAMP 'epoch' + se.ts/1000 \
+    * INTERVAL '1 second'       AS start_time,
+    lt.userId                   AS user_id,
+    lt.level                    AS level,
+    st.song_id                  AS song_id,
+    st.artist_id                AS artist_id,
+    lt.sessionId                AS session_id,
+    lt.location                 AS location,
+    lt.userAgent                AS user_agent
+    FROM log_table  AS lt
+    JOIN song_table AS st
+        ON (lt.artist = st.artist_name);
+""")
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    songplays_table.write.partitionBy("year", "month").parquet("{}/songplays.parquet".format(output_data))
 
 
 def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = "output"
+    output_data = "s3a://udacity-dend/output/"
 
     process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)

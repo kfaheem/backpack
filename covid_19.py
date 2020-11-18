6e33ed331400c37590816977ac9c579cf5266e2d
Faheem Kaskar <kfaheem@Faheems-MacBook-Pro.local>
2020-11-17 20:20:27 -0500

    webscraping

--- covid_19.py
+++ covid_19.py
@@ -3,16 +3,17 @@ import requests
 import logging
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from elasticsearch import helpers
+import sys
 
-
+logging.basicConfig(stream=sys.stdout)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def get_covid_data(url):
+def get_covid_data(URL):
     """
 
-    :param url:
+    :param URL:
     :return:
     """
     try:
@@ -24,24 +25,26 @@ def get_covid_data(url):
 
         logger.info("Initiating Scraping for Page Content")
         soup = BeautifulSoup(page.content, 'html.parser')
-        data_table = soup.find("table", attrs={"class": "waffle"})
 
-        t_headers = ['Patient Number', 'State Patient Number', 'Date Announced', 'Age Bracket',
-                     'Gender', 'Detected City', 'Detected District', 'Detected State', 'State Code', 'Current Status',
-                     'Notes', 'Contracted from which Patient (Suspected)', 'Nationality', 'Type of transmission',
-                     'Status Change Date', 'Source_1', 'Source_2', 'Source_3', 'Backup Notes']
-
-        for tr in data_table.tbody.find_all("tr"):
-            t_row = {}
-            for td, th in zip(tr.find_all("td"), t_headers):
-                t_row[th] = td.text.replace('\n', '').strip()
-            table_data.append(t_row)
-
-        table_data.remove(table_data[0])
-        table_data.remove(table_data[0])
+        print(soup.prettify())
+        # data_table = soup.find("table", attrs={"class": "waffle"})
+        #
+        # t_headers = ['Patient Number', 'State Patient Number', 'Date Announced', 'Age Bracket',
+        #              'Gender', 'Detected City', 'Detected District', 'Detected State', 'State Code', 'Current Status',
+        #              'Notes', 'Contracted from which Patient (Suspected)', 'Nationality', 'Type of transmission',
+        #              'Status Change Date', 'Source_1', 'Source_2', 'Source_3', 'Backup Notes']
+        #
+        # for tr in data_table.tbody.find_all("tr"):
+        #     t_row = {}
+        #     for td, th in zip(tr.find_all("td"), t_headers):
+        #         t_row[th] = td.text.replace('\n', '').strip()
+        #     table_data.append(t_row)
+        #
+        # table_data.remove(table_data[0])
+        # table_data.remove(table_data[0])
         logger.info("Scraping Complete")
-
-        return table_data
+        #
+        # return table_data
 
     except Exception as exception:
         logger.error("Received Exception in get_covid_data function - {}".format(exception))
@@ -69,29 +72,30 @@ def yield_es_data(covid_data):
         raise exception
 
 
-def lambda_handler(event, context):
+def main():
     """
 
-    :param event:
-    :param context:
     :return:
     """
     try:
-        covid_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSc_2y5N0I67wDU38DjDh35IZSIS30rQf7_' \
-              'NYZhtYYGU1jJYT6_kDx4YpF-qw0LSlGsBYP8pqM_a1Pd/pubhtml'
+        url = "https://www.mottchildren.org"
 
-        covid_data = get_covid_data(url=covid_url)
+        covid_data = get_covid_data(URL=url)
 
-        es_client = Elasticsearch(
-            hosts=[{"host": "", "port": 443}],
-            timeout=300,
-            use_ssl=True,
-            verify_certs=False,
-            connection_class=RequestsHttpConnection
-        )
-
-        es_response = helpers.bulk(es_client, yield_es_data(covid_data=covid_data))
-        logger.info("es_response - {}".format(es_response))
+        # es_client = Elasticsearch(
+        #     hosts=[{"host": "", "port": 443}],
+        #     timeout=300,
+        #     use_ssl=True,
+        #     verify_certs=False,
+        #     connection_class=RequestsHttpConnection
+        # )
+        #
+        # es_response = helpers.bulk(es_client, yield_es_data(covid_data=covid_data))
+        # logger.info("es_response - {}".format(es_response))
 
     except Exception as exception:
-        logger.error("Received Exception in lambda_handler - {}".format(exception))
+        logger.error("Received Exception in main function - {}".format(exception))
+
+
+if __name__ == "__main__":
+    main()
